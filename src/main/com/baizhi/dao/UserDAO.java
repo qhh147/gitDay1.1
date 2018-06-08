6917f96237fcfeaec9d07bb1ce11b69e65491327
qhh147 <m13426189440@163.com>
2018-06-08 16:07:43 +0800

    创建实体类接口

---
+++ src/main/com/baizhi/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/6/8.
+ */
+public interface UserDAO {
+    List<User> queryAll();
+}
