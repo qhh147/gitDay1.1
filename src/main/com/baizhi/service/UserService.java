88e738c072d2135f0a42cc0a6e185ec49e6a025d
qhh147 <m13426189440@163.com>
2018-06-08 17:15:24 +0800

    qqq修改service接口

--- src/main/com/baizhi/service/UserService.java
+++ src/main/com/baizhi/service/UserService.java
@@ -7,8 +7,6 @@ import java.util.List;
 /**
  * Created by Administrator on 2018/6/8.
  */
-public class UserService {
-    List<User> queryAll() {
-        return null;
-    }
+public interface UserService {
+    List<User> queryAll();
 }
