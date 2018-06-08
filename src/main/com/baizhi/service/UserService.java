bfe7b3626646ddabe992eaa317944577ec439f24
qhh147 <m13426189440@163.com>
2018-06-08 16:44:27 +0800

    qqq创建service接口

---
+++ src/main/com/baizhi/service/UserService.java
@@ -0,0 +1,14 @@
+package com.baizhi.service;
+
+import com.baizhi.entity.User;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/6/8.
+ */
+public class UserService {
+    List<User> queryAll() {
+        return null;
+    }
+}
